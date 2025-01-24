13d62015f12932d78d37f929296f5d6885afa8df
rendy eka febriyanto <rendimana12@gmail.com>
2025-01-24 17:20:49 +0700

    update next config

--- app/api/registerUser.js
+++ app/api/registerUser.js
@@ -2,8 +2,6 @@ import axios from "axios";
 
 export const registerUser = async (userData) => {
     try {
-        const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
-        console.log("Using Backend URL:", backendUrl);
         const response = await axios.post(`${process.env.NEXT_PUBLIC_BACKEND_URL}/adduser`, userData);
         console.log("response from add user = ", response.data)
         return response.data
